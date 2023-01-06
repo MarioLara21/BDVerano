ed5c71f456fe0fd0007284e6d203f46734992465
MarioLara21 <62675263+MarioLara21@users.noreply.github.com>
2023-01-05 22:53:08 -0600

    Cambios minimos

--- Interfaz/src/Controlador/sysConexion.java
+++ Interfaz/src/Controlador/sysConexion.java
@@ -7,21 +7,22 @@ import java.sql.*;
 
 public class sysConexion {
     
-    public static Connection obtConection(){
+    public static Connection obtConection() throws ClassNotFoundException, SQLException{
         
         try{
+            Class.forName("sysConexion");
             String myDb="jdbc:oracle:thin:@localhost:1521:DBPRUEBA";
             String user="ge";
             String password="ge";
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             java.sql.Connection connection= DriverManager.getConnection(myDb,user,password);
             return (Connection) connection;
-            
         }   catch(SQLException e){
             System.out.println("El error es: "+ e);
             return null;
-        }
-        
+        }   
     }
-    
+    public static ResultSet ejecutarStoredProcedure(CallableStatement cs) throws SQLException{
+        return cs.executeQuery();
+    }
 }
